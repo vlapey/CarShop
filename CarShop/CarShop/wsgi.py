de1310210877a7f8de8e1aa648587a9f61810e0c
Vlad <vlad.lapey@gmail.com>
2023-12-18 16:35:30 +0300

    initial commit

---
+++ CarShop/CarShop/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for CarShop project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CarShop.settings')
+
+application = get_wsgi_application()
