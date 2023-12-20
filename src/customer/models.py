2aa36957fd1dc6727140892d5b2dd4210783f4c5
Vlad <vlad.lapey@gmail.com>
2023-12-20 16:35:07 +0300

    created customer model

--- src/customer/models.py
+++ src/customer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_updated = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
