9f129463f76b2d43f0524df643a4891eefaa6232
Vlad <vlad.lapey@gmail.com>
2023-12-28 16:14:08 +0300

    created history models

--- history/models.py
+++ history/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from core.abstract_models import ModelProperties
+from dealer.models import Dealer
+from customer.models import Customer
+from vendor.models import Vendor
 
-# Create your models here.
+
+class CustomerDealerHistory(ModelProperties):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer_dealer_history')
+    dealer = models.ForeignKey(Dealer, on_delete=models.CASCADE, related_name='dealer_customer_history')
+
+    def __str__(self):
+        return f"customer {self.customer.name} dealer {self.dealer.name}"
+
+
+class DealerVendorHistory(ModelProperties):
+    dealer = models.ForeignKey(Dealer, on_delete=models.CASCADE, related_name='dealer_vendor_history')
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='vendor_dealer_history')
+
+    def __str__(self):
+        return f"dealer {self.dealer.name} vendor {self.vendor.name}"
