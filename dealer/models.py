2b249384f592f8531ffbecc20c32d73412ccede8
Vlad <vlad.lapey@gmail.com>
2023-12-28 15:12:36 +0300

    1. implemented Dealer and DealerCar models

--- dealer/models.py
+++ dealer/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from core.abstract_models import ModelProperties
+from django_countries.fields import CountryField
+from car.models import Car
 
-# Create your models here.
+
+class Dealer(ModelProperties):
+    name = models.CharField(max_length=30, unique=True)
+    location = CountryField(blank_label='select country')
+    balance = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class DealerCar(ModelProperties):
+    dealer = models.ForeignKey(Dealer, on_delete=models.CASCADE, related_name='dealer_dealer_car')
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='car_dealer_car')
+    amount = models.IntegerField(default=0)
+    price = models.FloatField(default=.0)
+
+    def __str__(self):
+        return f'{self.dealer.name} {self.car.model}'
