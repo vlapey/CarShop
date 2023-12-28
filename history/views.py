05297429c4375779f6a4a5671bc0bbb465be8f96
Vlad <vlad.lapey@gmail.com>
2023-12-28 16:19:46 +0300

    created views for history

--- history/views.py
+++ history/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import mixins
+from .models import CustomerDealerHistory, DealerVendorHistory
+from .serializers import CustomerDealerHistorySerializer, DealerVendorHistorySerializer
 
-# Create your views here.
+
+class CustomerDealerHistoryViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin,
+                                   generics.GenericAPIView):
+    queryset = CustomerDealerHistory
+    serializer_class = CustomerDealerHistorySerializer
+
+
+class DealerVendorHistoryViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin,
+                                 generics.GenericAPIView):
+    queryset = DealerVendorHistory
+    serializer_class = DealerVendorHistorySerializer
