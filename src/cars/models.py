3bf3816137a26a17ebb9eb71f105ebe50e7b9a7c
Vlad <vlad.lapey@gmail.com>
2024-01-08 17:15:27 +0300

    made torque and horsepower only positive in CarSpecification

--- src/cars/models.py
+++ src/cars/models.py
@@ -5,8 +5,8 @@ from core.abstract_models import ModelProperties
 class CarSpecification(ModelProperties):
     name = models.CharField(max_length=30, unique=True)
     engine = models.CharField(max_length=60)
-    horsepower = models.IntegerField(default=0)
-    torque = models.IntegerField(default=0)
+    horsepower = models.PositiveIntegerField(default=0)
+    torque = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name
