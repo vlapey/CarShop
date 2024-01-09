37744b2a6d44f65c9102929aef8222ef803bf7b8
Vlad <vlad.lapey@gmail.com>
2024-01-09 17:54:51 +0300

    changed buyers counter to only positive numbers

--- src/vendor/models.py
+++ src/vendor/models.py
@@ -5,6 +5,6 @@ from core.abstract_models import ModelProperties
 class Vendor(ModelProperties):
     name = models.CharField(max_length=30, unique=True)
     foundation_year = models.CharField(max_length=4)
-    buyers_counter = models.IntegerField(default=0)
+    buyers_counter = models.PositiveIntegerField(default=0)
 
 
