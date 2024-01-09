d3b00303eb63fbe82c62d65312222e4d3c8d538e
Vlad <vlad.lapey@gmail.com>
2024-01-09 17:53:26 +0300

    changed discount to only positive ints, and implemented validator for max value

--- src/loyalties/models.py
+++ src/loyalties/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from core.abstract_models import ModelProperties
 from src.cars.models import Car
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Loyalties(ModelProperties):
@@ -8,7 +9,7 @@ class Loyalties(ModelProperties):
     end_date = models.DateTimeField()
     name = models.CharField(max_length=30)
     description = models.TextField(default='')
-    discount = models.IntegerField(default=0)
+    discount = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100), MinValueValidator(0)])
     car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='car_promotion')
 
     def __str__(self):
