d6f04bb649c3a09b7f3fce1553a9dfd2de72cbdf
Vlad <vlad.lapey@gmail.com>
2024-01-11 18:19:20 +0300

    dropped extra "cars" file

--- src/cars/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Car, CarSpecification
-
-admin.site.register(Car)
-admin.site.register(CarSpecification)
--- src/cars/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import generics
-
-from .models import Car
-from .serializers import CarSerializer
-
-
-class CarAPIView(generics.ListAPIView):
-    queryset = Car.objects.all()
-    serializer_class = CarSerializer
