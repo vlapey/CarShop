2c416675d5fb7f654c5b4a03c600894e09de4b9d
Vlad <vlad.lapey@gmail.com>
2024-01-12 20:28:21 +0300

    added cat_type and color fields to Cars model

--- src/cars/models.py
+++ src/cars/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from core.abstract_models import ModelProperties
+from core.enums import Color, CarType
 
 
 class CarSpecification(ModelProperties):
@@ -8,14 +9,16 @@ class CarSpecification(ModelProperties):
     horsepower = models.IntegerField(default=0)
     torque = models.IntegerField(default=0)
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return f'{self.name}, {self.engine}, {self.horsepower}, {self.torque}'
 
 
 class Cars(ModelProperties):
     brand = models.CharField(max_length=60)
     specification = models.ForeignKey(CarSpecification, on_delete=models.CASCADE,
                                       related_name='specification_car')
+    car_type = models.CharField(max_length=20, choices=CarType.choices(), default='sedan')
+    color = models.CharField(max_length=10, choices=Color.choices(), default='black')
 
     def __str__(self):
         return self.brand
