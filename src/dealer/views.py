597869a0086cdeca6230e8ca5de09804fb056675
Vladislav <vlad.lapey@gmail.com>
2024-01-22 17:23:29 +0300

    added filter to Dealer model

--- src/dealer/views.py
+++ src/dealer/views.py
@@ -1,7 +1,9 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
 from .models import Dealer
 from .serializers import DealerSerializer
+from .filters import DealerFilter
 
 
 class DealerViewSet(mixins.ListModelMixin,
@@ -10,3 +12,5 @@ class DealerViewSet(mixins.ListModelMixin,
                     GenericViewSet):
     queryset = Dealer.objects.all()
     serializer_class = DealerSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = DealerFilter
