be31e93198aa80afd6e29a38a38b20f9ce2872bb
Vladislav <vlad.lapey@gmail.com>
2024-01-22 17:16:31 +0300

    added filter to Cars model

--- src/cars/views.py
+++ src/cars/views.py
@@ -1,6 +1,8 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .filters import CarsFilter
 
 from .models import Cars, CarSpecification
 from .serializers import CarSerializer, CarSpecificationSerializer
@@ -13,6 +15,8 @@ class CarViewSet(mixins.ListModelMixin,
     queryset = Cars.objects.all()
     serializer_class = CarSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = CarsFilter
 
 
 class CarSpecificationViewSet(mixins.ListModelMixin,
