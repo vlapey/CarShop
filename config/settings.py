972fc18cdf3258f48adab945decb410dc3c959c7
Vlad <vlad.lapey@gmail.com>
2024-01-30 17:54:26 +0300

    1. added host email string to env 2. created utils class in config

--- config/settings.py
+++ config/settings.py
@@ -147,6 +147,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
