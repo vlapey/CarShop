25f87089c8217169f828f6ee47dafff6de9c9cc9
Vlad <vlad.lapey@gmail.com>
2024-02-26 18:51:33 +0300

    refactored models in history

--- src/history/models.py
+++ src/history/models.py
@@ -3,13 +3,12 @@ from core.abstract_models import ModelProperties
 from src.dealer.models import Dealer
 from src.customer.models import Customer
 from src.vendor.models import Vendor
-from src.loyalties.models import VendorsLoyalties
+from src.loyalties.models import VendorsLoyalties, DealersLoyalties
 from src.car.models import Car
 
 
 class History(ModelProperties):
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    discount = models.ForeignKey(VendorsLoyalties, on_delete=models.CASCADE, related_name='vendors_discount')
     price = models.PositiveIntegerField(default=0)
 
     class Meta:
@@ -19,6 +18,7 @@ class History(ModelProperties):
 class CustomerDealerHistory(History):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer_dealer_history')
     dealer = models.ForeignKey(Dealer, on_delete=models.CASCADE, related_name='dealer_customer_history')
+    discount = models.ForeignKey(DealersLoyalties, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"customer {self.customer.name} dealer {self.dealer.name}"
@@ -27,6 +27,7 @@ class CustomerDealerHistory(History):
 class DealerVendorHistory(History):
     dealer = models.ForeignKey(Dealer, on_delete=models.CASCADE, related_name='dealer_vendor_history')
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='vendor_dealer_history')
+    discount = models.ForeignKey(VendorsLoyalties, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"dealer {self.dealer.name} vendor {self.vendor.name}"
